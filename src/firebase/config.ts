527e64077cfe83ed0c989f8e5ba9f3cd468cb882
modpr <modpr@example.com>
2026-04-15 20:42:48 +0200

    Configure XL DJENY to use RepKyrEDF Firebase project

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,10 @@
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyDq7M-7uIatlaEcZSKBSmlZkF9dqb6oW_8",
-  authDomain: "formulytics-app.firebaseapp.com",
-  projectId: "formulytics-app",
-  storageBucket: "formulytics-app.firebasestorage.app",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456",
+  apiKey: "AIzaSyDZHWEsUPDOYPmvCDAYQgfZgD4hFDk7biQ",
+  authDomain: "repkyredf.firebaseapp.com",
+  projectId: "repkyredf",
+  storageBucket: "repkyredf.firebasestorage.app",
+  messagingSenderId: "47412925656",
+  appId: "1:47412925656:web:c1095e4e57ab996defb3ad",
+  measurementId: "G-8G2TETS07C"
 };
